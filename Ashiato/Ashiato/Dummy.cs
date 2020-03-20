4eb2ab6ce448298e7af78648bf84a78e2d6d6068
Gen TAKAHASHI <kuramechan428@gmail.com>
2020-03-20 13:16:13 +0900

    プロジェクトの作成

---
+++ Ashiato/Ashiato/Dummy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwallowNest.Ashiato
+{
+	public class Dummy
+	{
+		public string Welcome() => "Welcome, Ashiato!";
+	}
+}
---
+++ Ashiato/AshiatoTests/DummyTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SwallowNest.Ashiato;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwallowNest.Ashiato.Tests
+{
+	[TestClass()]
+	public class DummyTests
+	{
+		[TestMethod()]
+		public void SampleTest()
+		{
+			Assert.AreEqual("Welcome, Ashiato!", new Dummy().Welcome());
+		}
+	}
+}

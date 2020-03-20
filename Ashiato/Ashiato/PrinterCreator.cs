cce7f14305bee4eff51d8e51be46e4dc9a5d20e5
Gen TAKAHASHI <kuramechan428@gmail.com>
2020-03-20 18:06:31 +0900

    Printer作成のヘルパー、PrinterCreatorの作成

---
+++ Ashiato/Ashiato/PrinterCreator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwallowNest.Ashiato
+{
+	public static class PrinterCreator
+	{
+		private static readonly string timeFormat = "yyyy/MM/dd HH:mm:ss";
+
+		/// <summary>
+		/// ログレベルを文字列にしたときの最大幅です。
+		/// </summary>
+		public static int MaxLengthOfLogLevelText { get; } =
+			Enum.GetNames(typeof(LogLevel)).Select(x => x.Length).Max();
+
+		/// <summary>
+		/// 1行用のLogPrintHandlerを作成します。
+		/// </summary>
+		/// <param name="printer"></param>
+		/// <returns></returns>
+		public static LogPrintHandler OneLinePrinter(Action<string> printer)
+		{
+			//2018/04/01 12:00:00  INFO > logText
+			//2018/04/01 12:00:00 DEBUG > logText
+			//みたいな感じになる
+
+			return (logText, logLevel) =>
+			{
+				var nowStr = DateTime.Now.ToString(timeFormat);
+				var lvlStr = $" {logLevel.ToString().PadLeft(MaxLengthOfLogLevelText)} > ";
+
+				var result = nowStr + lvlStr + logText;
+
+				printer(result);
+			};
+		}
+	}
+}
---
+++ Ashiato/AshiatoTests/PrinterCreatorTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SwallowNest.Ashiato;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwallowNest.Ashiato.Tests
+{
+	[TestClass()]
+	public class PrinterCreatorTests
+	{
+		/// <summary>
+		/// ログの出力先
+		/// </summary>
+		List<string> log;
+		Logger logger;
+		readonly string sampleText = "sample text";
+
+		void SamplePrinter(string logText, LogLevel logLevel)
+		{
+			log.Add(logText);
+		}
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			log = new List<string>();
+			logger = new Logger();
+		}
+
+		[TestMethod()]
+		public void OneLinePrinterTest()
+		{
+			logger.Printer += PrinterCreator.OneLinePrinter(logText => log.Add(logText));
+			logger.OutputLogLevel = LogLevel.DEBUG;
+
+			string nowStr = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+			string[] logs = new[]{
+				$"{nowStr} DEBUG > {sampleText}",
+				$"{nowStr}  INFO > {sampleText}",
+			};
+
+			logger.Debug(sampleText);
+			logger.Print(sampleText, LogLevel.INFO);
+
+			CollectionAssert.AreEqual(logs, log);
+		}
+	}
+}

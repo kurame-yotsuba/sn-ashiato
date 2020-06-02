a72f0bdf4da9df4895a365bb9f27bf34159a67c5
Gen TAKAHASHI <kuramechan428@gmail.com>
2020-06-02 23:15:44 +0900

    span使ってリファクタリング

--- Ashiato/Ashiato/LogUtility.cs
+++ Ashiato/Ashiato/LogUtility.cs
@@ -8,12 +8,19 @@ namespace SwallowNest.Ashiato
 	public static class LogUtility
 	{
 		private static readonly string timeFormat = "yyyy/MM/dd HH:mm:ss";
+		private static readonly string prompt = " > ";
 
 		/// <summary>
 		/// ログレベルを文字列にしたときの最大幅です。
 		/// </summary>
 		public static int MaxLengthOfLogLevelText { get; } =
 			Enum.GetNames(typeof(LogLevel)).Select(x => x.Length).Max();
+
+		// 各種range
+		private static readonly Range timeRange = 0..timeFormat.Length;
+		private static readonly Range levelRange =
+			(timeFormat.Length + 1)..(timeFormat.Length + 1 + MaxLengthOfLogLevelText);
+		private static readonly Range textRange = (timeFormat.Length + 1 + MaxLengthOfLogLevelText + prompt.Length)..;
 
 		/// <summary>
 		/// 1行用のLogPrintHandlerを作成します。
@@ -31,7 +38,7 @@ namespace SwallowNest.Ashiato
 				string nowStr = DateTime.Now.ToString(timeFormat);
 				string lvlStr = log.Level.ToString().PadLeft(MaxLengthOfLogLevelText);
 
-				string result = $"{nowStr} {lvlStr} > {log.Text}";
+				string result = $"{nowStr} {lvlStr}{prompt}{log.Text}";
 
 				printer(result);
 			};
@@ -64,19 +71,19 @@ namespace SwallowNest.Ashiato
 		{
 			log = default;
 			bool fail;
-			string[] textList = lineText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-			fail = textList.Length != 5;
+			fail = lineText.Length < timeFormat.Length + 1 + MaxLengthOfLogLevelText + prompt.Length;
 			if (fail) { return false; }
 
-			fail = !DateTime.TryParse($"{textList[0]} {textList[1]}", out DateTime time);
+			ReadOnlySpan<char> span = lineText.AsSpan();
+
+			fail = !DateTime.TryParse(span[timeRange], out DateTime time);
 			if (fail) { return false; }
 
-
-			fail = !Enum.TryParse(textList[2], out LogLevel level);
+			var hoge = span[levelRange].ToString();
+			fail = !Enum.TryParse(span[levelRange].ToString(), out LogLevel level);
 			if (fail) { return false; }
 
-			string text = textList[4];
+			string text = span[textRange].ToString();
 
 			log = new LogInfo(text, level, time);
 			return true;

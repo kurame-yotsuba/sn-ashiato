d4211e1523be44539e6d319ffa369ecde4a05631
kurame <kuramechan428@gmail.com>
2021-02-07 23:25:18 +0900

    サンプルプロジェクトの追加

---
+++ Ashiato/Ashiato.Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using SwallowNest.Ashiato;
+using System;
+
+namespace Ashiato.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Printer += LogUtility.OneLinePrinter(Console.WriteLine);
+            Log.OutputLogLevel = LogLevel.DEBUG;
+
+            Log.Trace("Trace");
+            Log.Debug("Debug");
+            Log.Info("Info");
+            Log.Warn("Warn");
+            Log.Error("Error");
+        }
+    }
+}
